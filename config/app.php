eb8f8ceb9c8546ec2e1d67cc0976027fb78781d9
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-09 23:14:38 -0400

    testing

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://ex-change-l.azurewebsites.net/'),
 
     /*
     |--------------------------------------------------------------------------
