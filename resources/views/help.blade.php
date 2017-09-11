78be5694a4fff86c51fdbd0844ef3ffc70951c05
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-10 23:09:07 -0400

    test icon

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="{{ asset('js/vue.js') }}"></script>
         <title>Ex-change</title>
-        <link rel="icon" href="{{asset('favicon.ico')}}"/>
+        <link rel="icon" type="image/png" href="{{asset('favicon.ico')}}"/>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
--- resources/views/newAccount.blade.php
+++ resources/views/newAccount.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Ex-change</title>
-        <link rel="icon" href="{{asset('favicon.ico')}}"/>
+        <link rel="icon" type="image/png" href="{{asset('favicon.ico')}}"/>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
