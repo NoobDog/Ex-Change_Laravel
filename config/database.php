979ad2c215a1f00d632c6bf35e73923ee308e09f
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-11 16:08:56 -0400

    test database connect

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'ex-change.database.windows.net',
+            'port' => '1433',
+            'database' => 'ex-change',
+            'username' => 'haitao.sun',
+            'password' => 'Daihaosj87128',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
