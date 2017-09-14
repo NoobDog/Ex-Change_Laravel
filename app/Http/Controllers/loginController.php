cd2ae08bb9fe3be06017e84e85ca8c600be2882b
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-13 20:58:31 -0400

    add use Session

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use DB;
+use Session;
 class loginController extends Controller
 {
 	// public function __construct()
