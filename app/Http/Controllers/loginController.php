257179d7f26615986fdec1a0938fea51e1deebe3
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-16 23:17:34 -0400

    test pass value

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -52,11 +52,11 @@ class loginController extends Controller
 			}
 
 		}
-		public function forgetPassword_checkSecurityQuestions(Request $request) {
+		public function forgetPassword_checkSecurityQuestions(Request $request , $userEmail) {
 			$inputAnswer1 = $request->input('forgetPassword_Answer1');
 			$inputAnswer2 = $request->input('forgetPassword_Answer2');
 
-			return 'inputAnswer1: '.$inputAnswer1.' inputAnswer2: '.$inputAnswer2.' userAnswer1'.$userAnswer1.' userAnswer2: '.$userAnswer2;
+			return 'inputAnswer1: '.$inputAnswer1.' inputAnswer2: '.$inputAnswer2.' userEmail'.$userEmail;
 		}
 
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -191,7 +191,7 @@
                   </form>
                 </div>
               @elseif (isset($forgetPassword_securityQuestion))
-                <form method="post" action="{{route('forgetPassword_checkSecurityQuestions',['userEmail'=>$userEmail])}}">
+                <form method="post" action="{{route('forgetPassword_checkSecurityQuestions',$userEmail)}}">
                   {{ csrf_field() }}
                   <p style="color:white;"><b>{{$userEmail}}</b></p>
                   <p style="color:white;"><b>{{$userQuestion1}}</b></p>
