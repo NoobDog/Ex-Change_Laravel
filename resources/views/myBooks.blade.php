14ead1014e78440e1e9b491ac162519ce04c9463
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-24 22:45:27 -0400

    testing-remove if

--- resources/views/myBooks.blade.php
+++ resources/views/myBooks.blade.php
@@ -193,10 +193,10 @@
                         <p>yes books</p>
                         @foreach ($userBooks as $userBook)
                             <p>{{ $userBook['bookName'] }}</p>
-                            @if ($userBook['bookImage'] != '')
+                    
                               <img src="{{asset(users/).Session::get('userEmail').$userBook['bookImage']}}"/>
-                            @endif
-          
+
+
                         @endforeach
                         <a href="{{route('getAddBookForm')}}"><button class ='myButton'>Add New book</button></a>
                     @endif
