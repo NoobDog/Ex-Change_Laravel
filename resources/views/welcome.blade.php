b909af375c510970242b9fbb9041a1f9c8754c50
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-27 19:38:00 -0400

    remove code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -296,11 +296,7 @@
                         <li><span>Image4</span></li>
                         <li><span>Image5</span></li>
                         <li><span>Image6</span></li>					
-                        <!-- @foreach ($books => $bookContent)
-                          @foreach ($bookContent => $book) 
-                          <li> <img src="{{asset('users').$book['bookImage']}}"><span>{{$book['bookName']}}</span></li>
-                          @endforeach
-                        @endforeach -->
+        
                       </ul>
                       <div class="clearfix"></div>
                       <a id="prev" class="prev" href="#">&lt;</a>
