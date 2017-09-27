514b5b27d4d48c62de605d8020923e18b6c7e72f
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-27 18:41:38 -0400

    add js code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,3 +181,13 @@
         </div>
     </body>
 </html>
+<script>
+var options = {
+	horizontal: 1,
+	itemNav: 'basic',
+	speed: 300,
+	mouseDragging: 1,
+	touchDragging: 1
+};
+var frame = new Sly('#frame', options).init();
+</script>
