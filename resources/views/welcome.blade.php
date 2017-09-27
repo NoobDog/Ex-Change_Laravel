603b8404285a55b911a1f7f9299f0cd1f6e9698d
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-27 11:24:28 -0400

    test item scroll bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -130,136 +130,100 @@
 
 
 
+            /* Example wrapper */
+            .wrap {
+            	position: relative;
+            	margin: 3em 0;
+            }
 
             /* Frame */
             .frame {
-              height: 250px;
-              line-height: 250px;
-              overflow: hidden;
+            	height: 250px;
+            	line-height: 250px;
+            	overflow: hidden;
             }
-
             .frame ul {
-              list-style: none;
-              margin: 0;
-              padding: 0;
-              height: 100%;
-              font-size: 50px;
+            	list-style: none;
+            	margin: 0;
+            	padding: 0;
+            	height: 100%;
+            	font-size: 50px;
             }
-
             .frame ul li {
-              float: left;
-              width: 227px;
-              height: 100%;
-              margin: 0 1px 0 0;
-              padding: 0;
-              background: #333;
-              color: #ddd;
-              text-align: center;
-              cursor: pointer;
+            	float: left;
+            	width: 227px;
+            	height: 100%;
+            	margin: 0 1px 0 0;
+            	padding: 0;
+            	background: #333;
+            	color: #ddd;
+            	text-align: center;
+            	cursor: pointer;
             }
-
             .frame ul li.active {
-              color: #fff;
-              background: #a03232;
+            	color: #fff;
+            	background: #12a5f4;
             }
-
 
             /* Scrollbar */
-
             .scrollbar {
-              margin: 0 0 1em 0;
-              height: 2px;
-              background: #ccc;
-              line-height: 0;
+            	margin: 0 0 1em 0;
+            	height: 2px;
+            	background: #ccc;
+            	line-height: 0;
             }
-
             .scrollbar .handle {
-              width: 100px;
-              height: 100%;
-              background: #292a33;
-              cursor: pointer;
+            	width: 100px;
+            	height: 100%;
+            	background: #292a33;
+            	cursor: pointer;
             }
-
             .scrollbar .handle .mousearea {
-              position: absolute;
-              top: -9px;
-              left: 0;
-              width: 100%;
-              height: 20px;
+            	position: absolute;
+            	top: -9px;
+            	left: 0;
+            	width: 100%;
+            	height: 20px;
             }
-
 
             /* Pages */
-
             .pages {
-              list-style: none;
-              margin: 20px 0;
-              padding: 0;
-              text-align: center;
+            	list-style: none;
+            	margin: 20px 0;
+            	padding: 0;
+            	text-align: center;
             }
-
             .pages li {
-              display: inline-block;
-              width: 14px;
-              height: 14px;
-              margin: 0 4px;
-              text-indent: -999px;
-              border-radius: 10px;
-              cursor: pointer;
-              overflow: hidden;
-              background: #fff;
-              box-shadow: inset 0 0 0 1px rgba(0, 0, 0, .2);
+            	display: inline-block;
+            	width: 14px;
+            	height: 14px;
+            	margin: 0 4px;
+            	text-indent: -999px;
+            	border-radius: 10px;
+            	cursor: pointer;
+            	overflow: hidden;
+            	background: #fff;
+            	box-shadow: inset 0 0 0 1px rgba(0,0,0,.2);
             }
-
             .pages li:hover {
-              background: #aaa;
+            	background: #aaa;
             }
-
             .pages li.active {
-              background: #666;
+            	background: #666;
             }
-
 
             /* Controls */
-
-            .controls {
-              margin: 25px 0;
-              text-align: center;
-            }
-
+            .controls { margin: 25px 0; text-align: center; }
 
             /* One Item Per Frame example*/
-
-            .oneperframe {
-              height: 300px;
-              line-height: 300px;
-            }
-
-            .oneperframe ul li {
-              width: 1140px;
-            }
-
-            .oneperframe ul li.active {
-              background: #333;
-            }
-
+            .oneperframe { height: 300px; line-height: 300px; }
+            .oneperframe ul li { width: 1140px; }
+            .oneperframe ul li.active { background: #333; }
 
             /* Crazy example */
-
-            .crazy ul li:nth-child(2n) {
-              width: 100px;
-              margin: 0 4px 0 20px;
-            }
-
-            .crazy ul li:nth-child(3n) {
-              width: 300px;
-              margin: 0 10px 0 5px;
-            }
-
-            .crazy ul li:nth-child(4n) {
-              width: 400px;
-              margin: 0 30px 0 2px;
-            }
+            .crazy ul li:nth-child(2n) { width: 100px; margin: 0 4px 0 20px; }
+            .crazy ul li:nth-child(3n) { width: 300px; margin: 0 10px 0 5px; }
+            .crazy ul li:nth-child(4n) { width: 400px; margin: 0 30px 0 2px; }
         </style>
     </head>
 
@@ -302,51 +266,40 @@
                         </div>
                     </span>
                   </div> -->
-<div class="wrap">
-  <div class="scrollbar">
-    <div class="handle">
-      <div class="mousearea"></div>
-    </div>
-  </div>
 
-  <div class="frame" id="basic">
-    <ul class="clearfix">
-      <li>0</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>11</li>
-      <li>12</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>18</li>
-      <li>19</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>25</li>
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-    </ul>
-  </div>
+<div class="pagespan container">
+		<div class="wrap">
 
-  <ul class="pages"></ul>
+			<div class="scrollbar">
+				<div class="handle">
+					<div class="mousearea"></div>
+				</div>
+			</div>
 
-</div>
+			<div class="frame" id="cyclepages">
+				<ul class="clearfix">
+					<li>Menu 0</li><li>Menu 1</li><li>Menu 2</li><li>Menu 3</li><li>Menu 4</li><li>Menu 5</li><li>Menu 6</li><li>Menu 7</li>
+					<li>Menu 8</li><li>Menu 9</li><li>Menu 10</li><li>Menu 11</li><li>Menu 12</li><li>Menu 13</li><li>Menu 14</li><li>Menu 15</li>
+					<li>Menu 16</li><li>Menu 17</li><li>Menu 18</li><li>Menu 19</li><li>Menu 20</li><li>Menu 21</li><li>Menu 22</li><li>Menu 23</li>
+					<li>Menu 24</li><li>Menu 25</li><li>Menu 26</li><li>Menu 27</li><li>Menu 28</li><li>Menu 29</li>
+				</ul>
+			</div>
+
+			<ul class="pages"></ul>
+
+			<div class="controls center">
+				<button class="btn prevPage"><i class="icon-chevron-left"></i><i class="icon-chevron-left"></i> Previous</button>
+
+				<!-- <div class="btn-group">
+					<button class="btn pause"><i class="icon-pause"></i> pause</button>
+					<button class="btn resume"><i class="icon-play"></i> resume</button>
+					<button class="btn toggle"><i class="icon-pause"></i> toggle</button>
+				</div> -->
+
+				<button class="btn nextPage">Next <i class="icon-chevron-right"></i><i class="icon-chevron-right"></i></button>
+			</div>
+		</div>
+	</div>
 
 
 
