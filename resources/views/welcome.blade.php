07c4580b56f07ef0050a11033116e69ccc97f06b
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-27 10:58:44 -0400

    remove buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -343,32 +343,6 @@
 
   <ul class="pages"></ul>
 
-  <div class="controls center">
-    <button class="btn prevPage"><i class="icon-chevron-left"></i><i class="icon-chevron-left"></i> page</button>
-    <button class="btn prev"><i class="icon-chevron-left"></i> item</button>
-    <button class="btn backward"><i class="icon-chevron-left"></i> move</button>
-
-    <div class="btn-group">
-      <button class="btn toStart">toStart</button>
-      <button class="btn toCenter">toCenter</button>
-      <button class="btn toEnd">toEnd</button>
-    </div>
-
-    <div class="btn-group">
-      <button class="btn toStart" data-item="10"><strong>10</strong> toStart</button>
-      <button class="btn toCenter" data-item="10"><strong>10</strong> toCenter</button>
-      <button class="btn toEnd" data-item="10"><strong>10</strong> toEnd</button>
-    </div>
-
-    <div class="btn-group">
-      <button class="btn add"><i class="icon-plus-sign"></i></button>
-      <button class="btn remove"><i class="icon-minus-sign"></i></button>
-    </div>
-
-    <button class="btn forward">move <i class="icon-chevron-right"></i></button>
-    <button class="btn next">item <i class="icon-chevron-right"></i></button>
-    <button class="btn nextPage">page <i class="icon-chevron-right"></i><i class="icon-chevron-right"></i></button>
-  </div>
 </div>
 
 
