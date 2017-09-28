2b94df1e82270a6b0e6e0ef0cc8c63f8bee8c67a
NoobDog <haitao.sun@mohawkcollege.ca>
2017-09-28 10:17:18 -0400

    handle when seesion logout and check user's books page

--- app/Http/Controllers/myBooksController.php
+++ app/Http/Controllers/myBooksController.php
@@ -8,18 +8,25 @@ use Illuminate\Support\Facades\Hash;
 use Session;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 class myBooksController extends Controller
 {
 
 		public function index() {
-			 \View::share(['page_name_active'=> 'myEx-change']);
-			 $user = DB::select('SELECT userID FROM users WHERE userEmail = ?', [Session::get('userEmail')]);
- 			 $user = json_decode(json_encode($user),true);
-			 $userID = $user[0]['userID'];
-			 $userBooks = DB::select('SELECT * FROM books WHERE userID = ?', [$userID]);
-			 $userBooks = json_decode(json_encode($userBooks),true);
-       return \View::make('myBooks',['userBooks' => $userBooks]);
-    }
+			 if(Session::has('userEmail')) {
+				\View::share(['page_name_active'=> 'myEx-change']);
+				$user = DB::select('SELECT userID FROM users WHERE userEmail = ?', [Session::get('userEmail')]);
+				$user = json_decode(json_encode($user),true);
+				$userID = $user[0]['userID'];
+				$userBooks = DB::select('SELECT * FROM books WHERE userID = ?', [$userID]);
+				$userBooks = json_decode(json_encode($userBooks),true);
+				return \View::make('myBooks',['userBooks' => $userBooks]);
+			 }
+			 else {
+				return redirect()->route('home');
+			 }
+
+		}
 		public function triggerAddNewBook() {
 				 return view('myBooks',['page_name_active'=> 'myEx-change','getAddBookForm'=>'true']);
 		}
