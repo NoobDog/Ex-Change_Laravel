be753fb2da2680a46b1ef9f016a626576670b5e8
NoobDog <haitao.sun@mohawkcollege.ca>
2017-10-05 13:28:24 -0400

    add book type

--- app/Http/Controllers/bookDetailController.php
+++ app/Http/Controllers/bookDetailController.php
@@ -12,8 +12,13 @@ class bookDetailController extends Controller
 
 		public function index($book) {
 			$book = DB::select('SELECT * FROM books WHERE bookID = ?',[$book]);
-			$book = json_decode(json_encode($book),true);
+            $book = json_decode(json_encode($book),true);
+            
+
             if(!empty($book)) {
+                $bookType = DB::select('SELECT typeName FROM bookTypes WHERE typeID = ?',[$book[0]['bookTypeID']]);
+                $bookType = json_decode(json_encode($bookType),true);
+                $book[0]['bookType'] = $bookType;
                 \View::share(['page_name_active'=> 'home','book'=>$book[0]]);
                 return \View::make('bookDetail'); 
             } else {
