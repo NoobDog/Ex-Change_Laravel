39db929b7a0521a1295d139908884b0c280d300f
NoobDog <haitao.sun@mohawkcollege.ca>
2017-10-05 18:07:46 -0400

    seesion testing

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel - Help
+                    {{print_r(session())}}
                 </div>
 
 
