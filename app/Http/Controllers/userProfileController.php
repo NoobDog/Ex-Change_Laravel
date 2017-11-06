0ca220744dea9863d2aab1af6a9283cf324ad466
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-06 13:52:31 -0500

    add session package

--- app/Http/Controllers/userProfileController.php
+++ app/Http/Controllers/userProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use DB;
+use Session;
 class userProfileController extends Controller
 {
 	// public function __construct()
