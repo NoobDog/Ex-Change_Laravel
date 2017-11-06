ad15757964ad9cd9dbe3878f0254cda9594b1a69
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-06 13:56:22 -0500

    fix data and style

--- app/Http/Controllers/userProfileController.php
+++ app/Http/Controllers/userProfileController.php
@@ -14,6 +14,8 @@ class userProfileController extends Controller
 	// }
 		public function index() {
 			$user = DB::select('SELECT * FROM users WHERE userID = ?', [Session::get('userID')]);
+			$user = json_decode(json_encode($user),true);
+			$user = $user[0];
 			\View::share(['page_name_active'=> 'home']);
             return \View::make('userProfile',['user' => $user]);
         }
--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -16,7 +16,7 @@
         <!--Font awesome-->
         <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}">
         <!-- Styles -->
-        <link rel="stylesheet" href="{{asset('css/login.css')}}">
+        <link rel="stylesheet" href="{{asset('css/userProfile.css')}}">
     </head>
     <body>
         @include('header')
