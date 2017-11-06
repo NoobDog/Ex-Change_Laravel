9885007af7348c858e8fde832b6e3f859b35045a
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-06 17:20:46 -0500

    commentout

--- app/Http/Controllers/userProfileController.php
+++ app/Http/Controllers/userProfileController.php
@@ -8,10 +8,7 @@ use DB;
 use Session;
 class userProfileController extends Controller
 {
-	// public function __construct()
-	// {
-	// 	View::share(['page_name_active'=> 'mypagename']);
-	// }
+
 		public function index() {
 			$user = DB::select('SELECT * FROM users WHERE userID = ?', [Session::get('userID')]);
 			$user = json_decode(json_encode($user),true);
@@ -19,17 +16,17 @@ class userProfileController extends Controller
 			\View::share(['page_name_active'=> 'home']);
             return \View::make('userProfile',['user' => $user]);
 		}
-		public function changePassword() {
-			return view('login',['page_name_active'=> 'home','changePassword'=>'true']);
-		}
-		public function setNewPassword(Request $request) {
-			$newPassword = $request->input('passWord');
-			$newPasswordHashed = md5($newPassword);
-			DB::update('UPDATE users SET userPassword = ? where userID = ?', [$newPasswordHashed, Session::get('userID')]);
-			$user = DB::select('SELECT * FROM users WHERE userID = ?', [Session::get('userID')]);
-			$user = json_decode(json_encode($user),true);
-			$user = $user[0];
-            return view('userProfile',['user' => $user,'page_name_active'=> 'home']);
+		// public function changePassword() {
+		// 	return view('login',['page_name_active'=> 'home','changePassword'=>'true']);
+		// }
+		// public function setNewPassword(Request $request) {
+		// 	$newPassword = $request->input('passWord');
+		// 	$newPasswordHashed = md5($newPassword);
+		// 	DB::update('UPDATE users SET userPassword = ? where userID = ?', [$newPasswordHashed, Session::get('userID')]);
+		// 	$user = DB::select('SELECT * FROM users WHERE userID = ?', [Session::get('userID')]);
+		// 	$user = json_decode(json_encode($user),true);
+		// 	$user = $user[0];
+        //     return view('userProfile',['user' => $user,'page_name_active'=> 'home']);
 
-		}
+		// }
 }
