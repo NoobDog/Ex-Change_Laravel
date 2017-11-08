a2f46ec77e5104e7434634d59cfeada1a062202f
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-08 13:07:50 -0500

    update address setting page

--- resources/views/addressSetting.blade.php
+++ resources/views/addressSetting.blade.php
@@ -27,17 +27,83 @@
             @else 
                 <img src="{{asset('icons/'.session()->get('userIcon'))}}" id="userIcon" style='width:200px; height:200px;'>
             @endif
+            <br>
             <label><b>{{session()->get('userName')}}</b></label>
             @if(isset($userAddress) && !empty($userAddress))
+                <form>
+                    <table>
+                        <tr>
+                            <th><label><b>Country:</b></label></th>
+                            <td>
+                                <select class='select-style' name='userCountry'>
+                                    @foreach($countries as $key => $val)
+                                        @if($key == $userAddress['userCountry'])
+                                            <option value="{{$key}}" selected>{{$val}}</option>
+                                        @else
+                                            <option value="{{$key}}">{{$val}}</option>
+                                        @endif
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
 
+                        <tr>
+                            <th><label><b>Province:</b></label></th>
+                            <td><input type="text" value="{{$userAddress['userProvince']}}" name="userProvince" required="required"/></td>
+                        </tr>
+
+                        <tr>
+                            <th><label><b>City:</b></label></th>
+                            <td><input type="text" value="{{$userAddress['userCity']}}" name="userCity" required="required"/></td>
+                        </tr>
+
+                        <tr>
+                            <th><label><b>Address:</b></label></th>
+                            <td><input type="text" value="{{$userAddress['userAddress']}}" name="userAddress" required="required"/></td>
+                        </tr>
+
+                        <tr>
+                            <th><label><b>Postal Code:</b></label></th>
+                            <td><input type="text" value="{{$userAddress['userPostalCode']}}" name="userPostalCode" required="required"/></td>
+                        </tr>
+                    </table>
+                </form>
+            @elseif(isset($userAddress))
+            <form>
                 <table>
                     <tr>
-                        <th><label><b>Password:</b></label></th>
-                        <td></td>
+                        <th><label><b>Country:</b></label></th>
+                        <td>
+                            <select class='select-style' name='userCountry'>
+                            <option value="-1">Please select a country</option>
+                                @foreach($countries as $key => $val)
+                                     <option value="{{$key}}">{{$val}}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th><label><b>Province:</b></label></th>
+                        <td><input type="text" name="userProvince" required="required"/></td>
+                    </tr>
+
+                    <tr>
+                        <th><label><b>City:</b></label></th>
+                        <td><input type="text" name="userCity" required="required"/></td>
+                    </tr>
+
+                    <tr>
+                        <th><label><b>Address:</b></label></th>
+                        <td><input type="text" name="userAddress" required="required"/></td>
+                    </tr>
+
+                    <tr>
+                        <th><label><b>Postal Code:</b></label></th>
+                        <td><input type="text" name="userPostalCode" required="required"/></td>
                     </tr>
                 </table>
-            @else
-
+            </form>
             @endif
             </div>
         </div>
