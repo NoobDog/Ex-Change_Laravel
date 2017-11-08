be375a3dfa29fd6b02c44f3a889fbc33dbdcf480
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-08 14:03:41 -0500

    add adminID option

--- app/Http/Controllers/addressSettingController.php
+++ app/Http/Controllers/addressSettingController.php
@@ -275,9 +275,9 @@ class addressSettingController extends Controller
 
 			$checkAddress = DB::select('SELECT * FROM address WHERE userID = ?' , [Session::get('userID')]);
 			if(empty($checkAddress)) {
-				DB::insert('INSERT INTO address (userID, userCountry, userProvince, userCity, userAddress, userPostalCode, isVoid)
-					values (?, ?, ?, ?, ?, ?, ?)',
-					[Session::get('userID'), $userCountry, $userProvince, $userCity, $userAddress, $userPostalCode, 0]
+				DB::insert('INSERT INTO address (userID, userCountry, userProvince, userCity, userAddress, userPostalCode, isVoid, adminID)
+					values (?, ?, ?, ?, ?, ?, ?, ?)',
+					[Session::get('userID'), $userCountry, $userProvince, $userCity, $userAddress, $userPostalCode, 0, 0]
 				);
 			}else {
 				DB::update('UPDATE address SET userCountry = ?, userProvince = ?, userCity = ?, 
