8425dda7ccd25e70b842bf590a5ba136b3f0946e
NoobDog <haitao.sun@mohawkcollege.ca>
2017-11-28 14:21:50 -0500

    add footer (testing)

---
+++ app/Http/Controllers/footerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Hash;
+use Session;
+use Illuminate\Http\RedirectResponse;
+class footerController extends Controller
+{ 
+
+	// 	public function index($book) {
+	// 		$book = DB::select('SELECT * FROM books WHERE bookID = ?',[$book]);
+    //         $book = json_decode(json_encode($book),true);
+            
+            
+
+    //         if(!empty($book)) {
+    //             $bookType = DB::select('SELECT typeName FROM bookTypes WHERE typeID = ?',[$book[0]['bookTypeID']]);
+    //             $bookType = json_decode(json_encode($bookType),true);
+    //             $book[0]['bookType'] = $bookType[0]['typeName'];
+    //             \View::share(['page_name_active'=> 'home','book'=>$book[0]]);
+    //             return \View::make('bookDetail'); 
+    //         } else {
+    //             return redirect()->route('home');
+    //         }
+           
+    // }
+
+
+}
