804d37f42beb9694ff9d6241ac34cf7b6c887add
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-06 13:04:16 -0500

    add jquery dialog

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,9 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.12.4.js"></script>
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet"  type="text/css" href="{{asset('css/juqery-ui.css')}}">
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/jquery-ui.js')}}"></script>
+
 
 <div id="dialog" title="Basic dialog">
   <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
