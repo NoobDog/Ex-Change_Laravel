533a50f80b240a4a2fd6cd9ad3327a619516ad50
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-06 13:13:48 -0500

    fix the error

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
-<link rel="stylesheet"  type="text/css" href="{{asset('css/juqery-ui.css')}}">
-<script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-<!-- <script src="{{asset('js/jquery-ui.js')}}"></script> -->
+<link rel="stylesheet"  type="text/css" href="{{asset('css/jquery-ui.css')}}">
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/jquery-ui.js')}}"></script>
 
 
 <div id="dialog" title="Basic dialog">
