64acc00709c035442a9fff10ec515155b7f7c843
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-06 12:50:37 -0500

    testing.....

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,115 +1,12 @@
 <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
 
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-<div id = "dialog">
-    <div class="chattingDiv">
-        <div class="chat_window">
-            <div class="top_menu">
-                <div class="buttons">
-                    <div class="button close"></div>
-                    <div class="button minimize"></div>
-                    <div class="button maximize"></div>
-                </div>
-                <div class="title">Chat</div>
-            </div>
-            <ul class="messages"></ul>
-            <div class="bottom_wrapper clearfix">
-                <div class="message_input_wrapper">
-                    <input class="message_input" placeholder="Type your message here..." />
-                </div>
-                <div class="send_message">
-                    <div class="icon"></div>
-                    <div class="text">Send</div>
-                </div>
-            </div>
-        </div>
-        <div class="message_template">
-            <li class="message">
-                <div class="avatar"></div>
-                <div class="text_wrapper">
-                    <div class="text"></div>
-                </div>
-            </li>
-        </div> 
-    </div> 
+<div id="dialog" title="Basic dialog">
+  <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
 </div>
-<button id = "opener">Open Dialog </button>
 <script>
-    // $(function() {
-    // $( "#dialog" ).dialog({
-    //     autoOpen: false, 
-    //     buttons: {
-    //         OK: function() {$(this).dialog("close");}
-    //     },
-    //     title: "Success",
-    //     position: {
-    //         my: "left center",
-    //         at: "left center"
-    //     }
-    // });
-    // $( "#opener" ).click(function() {
-    //     $( "#dialog" ).dialog( "open" );
-    // });
-    // });
+  $( function() {
     $( "#dialog" ).dialog();
-(function () {
-    var Message;
-    Message = function (arg) {
-        this.text = arg.text, this.message_side = arg.message_side;
-        this.draw = function (_this) {
-            return function () {
-                var $message;
-                $message = $($('.message_template').clone().html());
-                $message.addClass(_this.message_side).find('.text').html(_this.text);
-                $('.messages').append($message);
-                return setTimeout(function () {
-                    return $message.addClass('appeared');
-                }, 0);
-            };
-        }(this);
-        return this;
-    };
-    $(function () {
-        var getMessageText, message_side, sendMessage;
-        message_side = 'right';
-        getMessageText = function () {
-            var $message_input;
-            $message_input = $('.message_input');
-            return $message_input.val();
-        };
-        sendMessage = function (text) {
-            var $messages, message;
-            if (text.trim() === '') {
-                return;
-            }
-            $('.message_input').val('');
-            $messages = $('.messages');
-            message_side = message_side === 'left' ? 'right' : 'left';
-            message = new Message({
-                text: text,
-                message_side: message_side
-            });
-            message.draw();
-            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
-        };
-        $('.send_message').click(function (e) {
-            return sendMessage(getMessageText());
-        });
-        $('.message_input').keyup(function (e) {
-            if (e.which === 13) {
-                return sendMessage(getMessageText());
-            }
-        });
-        sendMessage('Hello Philip! :)');
-        setTimeout(function () {
-            return sendMessage('Hi Sandy! How are you?');
-        }, 1000);
-        return setTimeout(function () {
-            return sendMessage('I\'m fine, thank you!');
-        }, 2000);
-    });
-}.call(this));
+  } );
 </script>
