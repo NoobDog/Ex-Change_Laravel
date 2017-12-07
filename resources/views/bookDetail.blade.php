122e9d38be1bea2575472da6d289b3c65f39f5db
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-06 22:41:12 -0500

    fix missing )

--- resources/views/bookDetail.blade.php
+++ resources/views/bookDetail.blade.php
@@ -68,7 +68,7 @@
                                         @if ($message['senderID'] == Session::get('userID'))
                                             <li class="right clearfix">
                                                 <span class="chat-img pull-right">
-                                                    <img src="{{asset('icons/'.Session::get('userIcon')}}" alt="User Avatar">
+                                                    <img src="{{asset('icons/'.Session::get('userIcon'))}}" alt="User Avatar">
                                                 </span>
                                                 <div class="chat-body clearfix">
                                                     <div class="header">
@@ -97,104 +97,7 @@
                                             </li>
                                         @endif
                                     @endforeach
-                                    <!-- <li class="left clearfix">
-                                        <span class="chat-img pull-left">
-                                            <img src="https://bootdey.com/img/Content/user_3.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">John Doe</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 12 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                        <span class="chat-img pull-right">
-                                            <img src="https://bootdey.com/img/Content/user_1.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Sarah</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 13 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. 
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="left clearfix">
-                                        <span class="chat-img pull-left">
-                                            <img src="https://bootdey.com/img/Content/user_3.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">John Doe</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 12 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                        <span class="chat-img pull-right">
-                                            <img src="https://bootdey.com/img/Content/user_1.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Sarah</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 13 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. 
-                                            </p>
-                                        </div>
-                                    </li>                    
-                                    <li class="left clearfix">
-                                        <span class="chat-img pull-left">
-                                            <img src="https://bootdey.com/img/Content/user_3.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">John Doe</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 12 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                        <span class="chat-img pull-right">
-                                            <img src="https://bootdey.com/img/Content/user_1.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Sarah</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 13 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. 
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                        <span class="chat-img pull-right">
-                                            <img src="https://bootdey.com/img/Content/user_1.jpg" alt="User Avatar">
-                                        </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Sarah</strong>
-                                                <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> 13 mins ago</small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales at. 
-                                            </p>
-                                        </div>
-                                    </li>                     -->
+
                                 @endif
                                 </ul>
                             </div>
