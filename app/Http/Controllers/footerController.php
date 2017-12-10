b5b7072a25c7998a39b1a61e2fb972a7f0b0af76
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-10 16:42:38 -0500

    remove comment code

--- app/Http/Controllers/footerController.php
+++ app/Http/Controllers/footerController.php
@@ -11,17 +11,6 @@ class footerController extends Controller
 { 
 
 		public function index() {
-			// $book = DB::select('SELECT * FROM books WHERE bookID = ?',[$book]);
-            // $book = json_decode(json_encode($book),true);
-            // if(!empty($book)) {
-            //     $bookType = DB::select('SELECT typeName FROM bookTypes WHERE typeID = ?',[$book[0]['bookTypeID']]);
-            //     $bookType = json_decode(json_encode($bookType),true);
-            //     $book[0]['bookType'] = $bookType[0]['typeName'];
-            //     \View::share(['page_name_active'=> 'home','book'=>$book[0]]);
-            //     return \View::make('bookDetail'); 
-            // } else {
-            //     return redirect()->route('home');
-            // }
             if(Session::has('userID')) {
                 $messages = DB::select('SELECT * FROM negotiate WHERE receiverID = ? AND isRead = ?',[Session::get('userID'), 0]);
                 $messages = json_decode(json_encode($messages),true);
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,67 +6,6 @@
 
 {{action('footerController@index')}}
 <div id="dialog" hidden>
-    <!-- selected chat -->
-    <!-- <div class="col-md-8 bg-white ">
-        <div class="chat-message">
-            <ul class="chat">
-            @if (count($messages) > 0)
-                @foreach ($messages as $message)
-                    @if ($message['senderID'] == Session::get('userID'))
-                        <li class="right clearfix">
-                            <span class="chat-img pull-right">
-                                <img src="{{asset('icons/'.Session::get('userIcon'))}}" alt="User Avatar">
-                            </span>
-                            <div class="chat-body clearfix">
-                                <div class="header">
-                                    <strong class="primary-font">{{Session::get('userName')}}</strong>
-                                    <small class="pull-right text-muted"><i class="fa fa-clock-o"></i> {{$message['date']}}</small>
-                                </div>
-                                <p>
-                                    {{$message['message']}}
-                                </p>
-                            </div>
-                        </li>
-                    @else
-                        <li class="left clearfix">
-                            <span class="chat-img pull-left">
-                                <img src="{{asset('icons/'.$book['bookUserIcon'])}}" alt="User Avatar">
-                            </span>
-                            <div class="chat-body clearfix">
-                                <div class="header">
-                                    <strong class="primary-font">{{$book['bookUserName']}}</strong>
-                                    <small class="pull-right text-muted"><i class="fa fa-clock-o"></i>{{$message['date']}}</small>
-                                </div>
-                                <p>
-                                {{$message['message']}}
-                                </p>
-                            </div>
-                        </li>
-                    @endif
-                
-                @endforeach
-
-            @endif
-            </ul>
-        </div>
-        <div class="chat-box bg-white">
-            <div class="input-group">
-                <form method="post" action="{{route('bookDetailAddMessage',$book['bookID'])}}">
-                {{csrf_field()}}
-                <input name="senderID" value="{{Session::get('userID')}}" hidden>
-                <input name="receiverID" value="{{$book['userID']}}" hidden>
-                <input name="sellerID" value="{{$book['userID']}}" hidden>
-                <input name="buyerID" value="{{Session::get('userID')}}" hidden>
-                    <input name="message" id ="message" class="textarea" placeholder="Type your message here">
-                    <span class="input-group-btn">
-                        <button class="sendBtn" type="submit">Send</button>
-                    </span>
-                </form>
-            </div>
-        </div>
-    </div>    -->
-
-
 </div>
 
 <div class="footBar">
