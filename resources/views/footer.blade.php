cc4f529c25b3712dafe64be15e3cb6479b72610f
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-11 15:55:22 -0500

    test

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -76,7 +76,7 @@
                     // HTML += '<input name="buyerID" value="'+buyerID+'" hidden>';
 
                     HTML += '<span class="input-group-btn">';
-                    HTML += '<button class="sendBtn" onClick="submit(senderID,sellerID,buyerID)">Send</button>'
+                    HTML += '<button class="sendBtn" onClick="submit('+senderID+','+sellerID+','+buyerID+')">Send</button>'
                     HTML += '</span>';
                     HTML += '</div>';
                     HTML += '</div>';
