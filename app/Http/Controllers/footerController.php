a4c143748d9dc1b3950fe2eb4431ad526b6007b8
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-20 15:14:05 -0500

    test

--- app/Http/Controllers/footerController.php
+++ app/Http/Controllers/footerController.php
@@ -52,13 +52,13 @@ class footerController extends Controller
                     return 'sameUser';
                 }
 
-
-                $shoppingCartItem = DB::select("SELECT * FROM shoppingCart WHERE bookID = ? AND userID = ?",[$bookID, Session::get('userID')]);
+                //$shoppingCartItem = DB::select("SELECT * FROM shoppingCart WHERE bookID = ? AND userID = ?",[$bookID, Session::get('userID')]);
+                $shoppingCartItem = DB::select("SELECT * FROM shoppingCart");
                 $shoppingCartItem = json_decode(json_encode($shoppingCartItem),true);
                 $shoppingCartItem = $shoppingCartItem[0];
                 //debug
                 return $shoppingCartItem;
-                
+
                 if(!empty($shoppingCartItem)) {
                     return 'hasInCart';
                 } else {
