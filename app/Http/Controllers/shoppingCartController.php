e23da8f2b410f865c72206223b54620410619521
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-26 21:39:20 -0500

    fix error

--- app/Http/Controllers/shoppingCartController.php
+++ app/Http/Controllers/shoppingCartController.php
@@ -13,7 +13,7 @@ class shoppingCartController extends Controller
 		public function index() {
 			$shoppingCart = DB::select('SELECT sc.*, b.bookTitle, b.bookImage, b.bookName, b.bookDescription FROM shoppingCart sc LEFT JOIN books b ON b.bookID = sc.bookID WHERE sc.userID = ?', [Session::get('userID')]);
 			$shoppingCart = json_decode(json_encode($shoppingCart),true);
-			$userCards = DB::select('SELECT * FROM creditCard WHERE userID = ? AND isConfirmed = ?, isVoid = ?', [Session::get('userID'), 1, 0]);
+			$userCards = DB::select('SELECT * FROM creditCard WHERE userID = ? AND isConfirmed = ? AND isVoid = ?', [Session::get('userID'), 1, 0]);
 			$userCards = json_decode(json_encode($userCards),true);
 			\View::share(['page_name_active'=> 'cart']);
 
