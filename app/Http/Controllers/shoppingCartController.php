79c41705e8a26b1181493c2ddd1b432a9b0179ba
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-28 22:56:26 -0500

    comment out

--- app/Http/Controllers/shoppingCartController.php
+++ app/Http/Controllers/shoppingCartController.php
@@ -65,7 +65,7 @@ class shoppingCartController extends Controller
 							[Session::get('userID'),$cardNumber,'null', $expiryYear.'-'.$expiryMonth,$nameOnCard,1,0,$cvv]
 						);
 						foreach ($shoppingCart as $cartItem) {
-							DB::update('UPDATE shoppingCart SET status = ? where bookID = ?', ['userPaid', $cartItem['bookID']]);
+							//DB::update('UPDATE shoppingCart SET status = ? where bookID = ?', ['userPaid', $cartItem['bookID']]);
 							$userCard = DB::select('SELECT * FROM creditCard WHERE userID = ?', [$cartItem['bookUser']]);
 							$userCard = json_decode(json_encode($userCard),true)[0];
 							if(!empty($userCard)) {
