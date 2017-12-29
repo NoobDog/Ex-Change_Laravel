be2ded8960a03de063f29f7c7aad7a9953df5256
NoobDog <haitao.sun@mohawkcollege.ca>
2017-12-29 06:26:32 -0500

    test

--- app/Http/Controllers/shoppingCartController.php
+++ app/Http/Controllers/shoppingCartController.php
@@ -55,7 +55,10 @@ class shoppingCartController extends Controller
 						"country" => "CA",
 						"email" => Session::get('userEmail')
 					));
-					return $newAccount;
+
+					$account = \Stripe\Account::retrieve($newAccount['id']);
+					$account->external_accounts->create(array("external_account" => $tok));
+					return $account;
 				}
 
 
