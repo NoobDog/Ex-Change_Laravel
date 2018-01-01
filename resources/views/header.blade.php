64986e4bc8cba17dae093349c46be0956a4ac12b
NoobDog <haitao.sun@mohawkcollege.ca>
2018-01-01 14:57:26 -0500

    testing admin

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,7 +3,7 @@
 <div class="logo">
   <img src="{{asset('img/ex-change_logo.jpg')}}" height="66px" width="100%">
 </div>
-@if (Session::has('userName'))
+@if (Session::has('userName') && Session::get('roleTypeID') == 1)
 <div class="navBar">
   <ul class="nb">
     <li class="nbDropdown">
@@ -34,6 +34,22 @@
       </div>
     </li> -->
     <li style="float:right;"><a href="{{url('/shoppingCart')}}" class="@if($page_name_active=='cart')active @endif"><i class="fa fa-shopping-cart fa-1x" aria-hidden="true"></i></a></li>
+  </ul>
+</div>
+@else if(Session::has('userName') && Session::get('roleTypeID') == 2)
+<div class="navBar">
+  <ul class="nb">
+    <li class="nbDropdown">
+      <a href="{{url('/')}}" class="dropbtn @if($page_name_active=='home')active @endif">Welcome : {{Session::get('userName')}}</a>
+      <div class="nbDropdown-content">
+        <a href="{{url('/userProfile')}}"><i class="fa fa-user" aria-hidden="true"></i> User Profile</a>
+        <!-- <a href="{{url('/general')}}"><i class="fa fa-info-circle" aria-hidden="true"></i> General</a> -->
+        <a href="{{url('/addressSetting')}}"><i class="fa fa-map-marker" aria-hidden="true"></i> Address Settings</a>
+        <a href="{{url('/privacySetting')}}"><i class="fa fa-lock" aria-hidden="true"></i> Privacy Settings</a>
+      </div>
+    </li>
+    <li><a class="@if($page_name_active=='help')active @endif" href="{{url('/help')}}">Help</a></li>
+    <li><a href="{{route('logout')}}">Logout</a></li>
   </ul>
 </div>
 @else
