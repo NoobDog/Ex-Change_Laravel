c883e070eb2f8417b72f833f83143d7484946a9b
NoobDog <haitao.sun@mohawkcollege.ca>
2018-01-02 21:29:04 -0500

    use db

--- app/Http/Controllers/helpController.php
+++ app/Http/Controllers/helpController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
-//use Stripe;
+use DB;
 class helpController extends Controller
 {
 	// public function __construct()
