5abc8afaf0c3f26c8626980ab687ec080675d949
NoobDog <haitao.sun@mohawkcollege.ca>
2018-03-25 17:30:03 -0400

    fix

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,13 +7,12 @@
 </head>
 <div class="logo">
   <img src="{{asset('img/ex-change_logo.jpg')}}" height="66px" width="100%">
-  <div class="imgTxt">Ex-Change</div>
 </div>
 <div>
-  <ul class="nav navbar navbar-dark bg-dark">
-    <li>
-      <a class="btn btn-secondary dropdown-toggle">Welcome</a>
-      <div class="dropdown">
+  <ul class="nav nav-tabs navbar-dark bg-dark">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Welcome</a>
+      <div class="dropdown-menu">
         <a class="dropdown-item"><i class="fa fa-user" aria-hidden="true"></i> User Profile</a>
         <!-- <a href="{{url('/general')}}"><i class="fa fa-info-circle" aria-hidden="true"></i> General</a> -->
         <a class="dropdown-item"><i class="fa fa-map-marker" aria-hidden="true"></i> Address Settings</a>
@@ -21,17 +20,17 @@
       </div>
     </li>
     <li>
-      <a class="btn btn-secondary dropdown-toggle">My Ex-change</a>
-      <div class="dropdown">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">My Ex-change</a>
+      <div class="dropdown-menu">
         <a class="dropdown-item"><i class="fa fa-book" aria-hidden="true"></i> My Books</a>
         <a class="dropdown-item"><i class="fa fa-university" aria-hidden="true"></i> My Bank Information</a>
         <a class="dropdown-item"><i class="fa fa-trademark" aria-hidden="true"></i> My Trade Information</a>
         <!-- <a href="#"><i class="fa fa-tint" aria-hidden="true"></i>  My Points</a> -->
       </div>
     </li>
-    <li><a>Help</a></li>
-    <li><a>Logout</a></li>
-    <li><i class="fa fa-shopping-cart fa-1x" aria-hidden="true"></i></a></li>
+    <li class="nav-item"><a>Help</a></li>
+    <li class="nav-item"><a>Logout</a></li>
+    <li class="nav-item"><i class="fa fa-shopping-cart fa-1x" aria-hidden="true"></i></a></li>
   </ul>
 </div>
 </html>
