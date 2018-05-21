b551485422a19a8120b99758d3a49476085263c0
NoobDog <haitao.sun@mohawkcollege.ca>
2018-05-21 00:05:36 -0400

    test a

--- app/Http/Controllers/weChatController.php
+++ app/Http/Controllers/weChatController.php
@@ -9,27 +9,27 @@ use Illuminate\Http\RedirectResponse;
 class weChatController extends Controller
 { 
 
-		public function saveToDataBase(Request $request) {
+		public function saveToDataBase() {
       if($request->ajax()){
         return "AJAX";
     }
     return "HTTP";
-      $name = $request->input('userName') ?? '';
-      $country = $request->input('userCountry') ?? '';
-      $IP = $request->input('userIP') ?? '';
-      try {
-        DB::insert(
-          'INSERT INTO weChat (userName, userCountry, userIP) values (?, ?, ?)',
-          [$name, $country, $IP]
-        );
-        return 'Got it~';
-      }
-      catch (Exception $e) {
-					// Something else happened, completely unrelated to Stripe
-					$body = $e->getJsonBody();
-          $err  = $body['error'];
-          return $err;
-      }
+    //   $name = $request->input('userName') ?? '';
+    //   $country = $request->input('userCountry') ?? '';
+    //   $IP = $request->input('userIP') ?? '';
+    //   try {
+    //     DB::insert(
+    //       'INSERT INTO weChat (userName, userCountry, userIP) values (?, ?, ?)',
+    //       [$name, $country, $IP]
+    //     );
+    //     return 'Got it~';
+    //   }
+    //   catch (Exception $e) {
+		// 			// Something else happened, completely unrelated to Stripe
+		// 			$body = $e->getJsonBody();
+    //       $err  = $body['error'];
+    //       return $err;
+    //   }
 
     }
 
