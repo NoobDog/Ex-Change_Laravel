1e757d191f6b44e5ec9a1d409dc1922d64b06851
NoobDog <haitao.sun@mohawkcollege.ca>
2018-05-21 00:24:52 -0400

    test

--- resources/views/weChat.blade.php
+++ resources/views/weChat.blade.php
@@ -1,5 +1,20 @@
 <html>
 <body>
 {{ csrf_token() }}
+<button onClick="test()">TEst</button>
+<script>
+function test () {
+  $.ajax({
+        type: "POST",
+        url: 'http://ex-change-l.azurewebsites.net/weChat',
+        beforeSend: function(xhr){xhr.setRequestHeader('X-CSRF-TOKEN', '{{ csrf_token() }}')}, 
+        data: {bookID: '1'},
+        success: function(msg) {
+          console.log(msg);
+        }
+    });
+}
+
+</script>
 </body>
 </html>
